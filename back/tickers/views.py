c030d11442b69b4227b9a1f35256b3f63e148ae3
Alexey Ustinnikov <gr4k@bk.ru>
2022-07-27 22:33:59 +0300

    add some comments

--- back/tickers/views.py
+++ back/tickers/views.py
@@ -6,10 +6,12 @@ from tickers.models import Ticker
 
 
 class IndexView(TemplateView):
+    """Ендпоинт для мониторинга работы бэка."""
     template_name = 'index.html'
 
 
 class TickerNamesView(View):
+    """Ендпоинт для заполнения селекта на фронте."""
     def get(self, request):
         ticker_names_list = Ticker.objects.values_list('name', flat=True)
         return JsonResponse([x for x in ticker_names_list], safe=False)
